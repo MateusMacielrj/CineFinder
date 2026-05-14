2d5f4d1cdc87d231d61d13ab5e2323b2dd6e7380
Mateus Maciel <mateusmacielrj@gmail.com>
2026-05-14 16:21:26 -0300

    fix: base path github pages

--- movie-app/vite.config.js
+++ movie-app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/CineFinder/", // 👈 ESSA LINHA RESOLVE A TELA BRANCA
 })
